ea96b19002cdb8adefb1a040f454679973e3784e
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-03 22:19:46 -0700

    Added more javaScript including API list

--- js/main.js
+++ js/main.js
@@ -18,14 +18,19 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const drawCards = `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=52`
+  let player1 = `https://deckofcardsapi.com/api/deck/${deck}/pile/player1/add/?cards=AS,2S`
+  let player1List =`https://deckofcardsapi.com/api/deck/${deck}/pile/player1/list/`
+  let player2 = `https://deckofcardsapi.com/api/deck/${deck}/pile/player2/add/?cards=AS,2S`
 
   fetch(drawCards)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-      
+        let firstHalf = data.code
+        console.log(firstHalf)
 
-        console.log(data)
-        console.log(drawCards)
+        console.log(player1List)
+        // console.log(data)
+        console.log(player1)
 
       })
       .catch(err => {
