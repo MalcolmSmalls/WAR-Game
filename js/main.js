86b26d237e6a8469cfccd07cef0478d0fbf2f281
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-03 21:01:04 -0700

    Updated JavaScript and added some CSS styling

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
-//Example fetch using pokemonapi.co
+
+const newGame = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+let deck = ""
+
+  fetch(newGame)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          deck = data.deck_id
+          console.log(deck)
+
+
+      })
+        .catch(err => {
+          console.log(`error ${err}`)
+      });
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const newGame = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+  const drawCards = `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=52`
 
-
-  fetch(newGame)
+  fetch(drawCards)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        let deck = data.deck_id
+      
 
+        console.log(data)
+        console.log(drawCards)
 
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+      
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
-const drawCards = `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=52`
+
 
